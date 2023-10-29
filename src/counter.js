6acd05781aa2bb367f78223d97d92caec2cd4bd5
Guest <guest-rk1lyc@h21655.sgb.skir.kr>
2023-10-29 18:14:05 +0900

    edit counter

--- src/counter.js
+++ src/counter.js
@@ -1,8 +1,19 @@
-const d_day = (enlist_day) => {
-    return Date.now() - enlist_day
-}
 
-console.log(Date.now(), 1665869081)
-console.log(d_day(1665869081))
-1665869081
-1698560856
+
+function DateDifference(date1, date2) {
+    const diffInMs = Math.abs(date2 - date1);
+  
+    const diffInSeconds = Math.floor(diffInMs / 1000);
+    const diffInMinutes = Math.floor(diffInSeconds / 60);
+    const diffInHours = Math.floor(diffInMinutes / 60);
+    const diffInDays = Math.floor(diffInHours / 24);
+  
+    return diffInDays;
+  }
+  
+const date1 = new Date('2022-10-16');
+const date2 = new Date(); // Current date
+
+const differenceInDays = DateDifference(date1, date2);
+console.log('Difference in days:', differenceInDays);
+
