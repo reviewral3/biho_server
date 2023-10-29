580c9d56a794961cda626c3d33d18a23840910ad
Guest <guest-rk1lyc@h21655.sgb.skir.kr>
2023-10-29 19:22:40 +0900

    set test on counter

--- src/counter.js
+++ src/counter.js
@@ -21,19 +21,22 @@ function DischargeDate(my_enlist_date) {
     var futureDate = new Date(my_enlist_date.getFullYear(), my_enlist_date.getMonth() + 18, my_enlist_date.getDate());
     return futureDate;
 }
-  
-const my_enlist_date = new Date('2022-10-17');
-const my_discharge_date = DischargeDate(my_enlist_date) 
-const current_date = new Date(); 
-
-const after_enlist = DateDifference(my_enlist_date, current_date);
-const until_discharge = DateDifference(my_discharge_date, current_date);
-
-console.log('after', after_enlist);
-console.log('rest_date', until_discharge);
 
 
-console.log(DischargeDate(my_enlist_date));
+function test() {
+    const my_enlist_date = new Date('2022-10-17');
+    const my_discharge_date = this.DischargeDate(my_enlist_date) 
+    const current_date = new Date(); 
+    
+    const after_enlist = this.DateDifference(my_enlist_date, current_date);
+    const until_discharge = this.DateDifference(my_discharge_date, current_date);
+    
+    console.log('after', after_enlist);
+    console.log('rest_date', until_discharge);
+    
+    console.log(this.DischargeDate(my_enlist_date));    
+}
+
 
 module.exports = {
     DateDifference, 
